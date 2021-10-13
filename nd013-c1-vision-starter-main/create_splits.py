89ede4d5bdaf3be4fda286dc9e77d5a13382d587
Carl <ostcarl@student.chalmers.se>
2021-10-13 14:18:31 +0200

    create_splits.py created

--- nd013-c1-vision-starter-main/create_splits.py
+++ nd013-c1-vision-starter-main/create_splits.py
@@ -6,18 +6,40 @@ import random
 import numpy as np
 
 from utils import get_module_logger
-
-
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import shutil
 def split(data_dir):
     """
     Create three splits from the processed records. The files should be moved to new folders in the 
     same directory. This folder should be named train, val and test.
-
     args:
         - data_dir [str]: data directory, /mnt/data
     """
-    # TODO: Implement function
+
+    try:
+        files = [filename for filename in glob.glob(f'{data_dir}/*.tfrecord')]
+    except Exception as err:
+        print("Unable to access file")
+    np.random.shuffle(files)
     
+    train_files, val_file, test_file = np.split(files, [int(.75*len(files)), int(.9*len(files))])
+    print(len(train_files), len(val_file), len(test_file))
+    train = os.path.join(data_dir, 'train')
+    os.makedirs(train, exist_ok=True)
+
+    for file in train_files:
+        shutil.move(file, train)
+    val = os.path.join(data_dir, 'val')
+    os.makedirs(val,exist_ok=True)
+    
+    for file in val_file:
+        shutil.move(file, val)
+    
+    test = os.path.join(data_dir, 'test')
+    os.makedirs(test, exist_ok=True)
+    for file in test_file:
+        shutil.move(file, test) 
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
